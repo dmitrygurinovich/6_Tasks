fde07b6d6b2e7df1a166b93fad32e73a69bc9543
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-05-07 22:24:41 +0300

    Task's done

--- Task1/src/main/java/by/epam/task/view/Menu.java
+++ Task1/src/main/java/by/epam/task/view/Menu.java
@@ -3,7 +3,6 @@ package by.epam.task.view;
 import java.util.Scanner;
 
 import by.epam.task.entity.Library;
-import by.epam.task.entity.User;
 import by.epam.task.entity.UserRole;
 
 public class Menu {
@@ -13,58 +12,20 @@ public class Menu {
 	}
 
 	public void adminMenu() {
-		System.out.println("+++ ADMIN MENU +++");
+		System.out.println("" +
+				"+++ ADMIN MENU +++\n" +
+				"1. Show catalog\n" +
+				"2. Search book\n" +
+				"3. Edit book\n" +
+				"0. Exit");
 	}
 
 	public void userMenu() {
-		System.out.println("+++ USER MENU +++");
-	}
-
-	public void createUser(Library library) {
-		String name;
-		String login;
-		String password;
-		UserRole role;
-
-		System.out.println("Enter name: ");
-		while (!in.hasNextLine()) {
-			in.next();
-			System.out.println("Enter name: ");
-		}
-		name = in.nextLine();
-
-		System.out.println("Enter login: ");
-		while (!in.hasNextLine()) {
-			in.next();
-			System.out.println("Enter login: ");
-		}
-		login = in.nextLine();
-
-		System.out.println("Enter password: ");
-		while (!in.hasNextLine()) {
-			in.next();
-			System.out.println("Enter password: ");
-		}
-		password = in.nextLine();
-
-		System.out.println("Set up user role: ");
-		System.out.println("1. User: ");
-		System.out.println("2. Administrator: ");
-		while (!in.hasNextInt() && (in.nextInt() != 1 || in.nextInt() != 2)) {
-			in.next();
-			System.out.println("Set up user role: ");
-			System.out.println("1. User");
-			System.out.println("2. Administrator");
-		}
-
-		if (in.nextInt() == 1) {
-			role = UserRole.USER;
-		} else {
-			role = UserRole.ADMINISTRATOR;
-		}
-
-		library.getUsers().add(new User(name, login, password, role));
-		System.out.println("User " + name + " added!");
+		System.out.println("" +
+				"+++ USER MENU +++\n" +
+				"1. Show catalog\n" +
+				"2. Search book\n" +
+				"0. Exit");
 	}
 
 	public void authorisation(Library library) {
