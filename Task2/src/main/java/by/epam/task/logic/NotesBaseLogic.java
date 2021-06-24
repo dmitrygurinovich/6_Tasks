a243b019d1c4d911fcb4b9eee2d546ea837b2327
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-06-24 21:18:12 +0300

    Task's done

--- Task2/src/main/java/by/epam/task/logic/NotesBaseLogic.java
+++ Task2/src/main/java/by/epam/task/logic/NotesBaseLogic.java
@@ -1,44 +1,51 @@
 package by.epam.task.logic;
 
 import by.epam.task.entity.Note;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.util.Calendar;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class NotesBaseLogic {
+    private final File noteBasePath;
+
     public NotesBaseLogic() {
-
+        this.noteBasePath = new File("Task2/src/main/resources/notesbase.json");
     }
 
-    public void writeNotesToFile() {
-
+    public void writeNotesToFile(ArrayList<Note> notes) {
+        try (FileWriter writer = new FileWriter(noteBasePath)) {
+            writer.write(objectToJsonObject(notes));
+            writer.flush();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
-    public void readNotesFromFile(){
+    public ArrayList<Note> readNotesFromFile() {
+        ArrayList<Note> notes;
+        notes = new ArrayList<>();
 
+        try(FileReader reader = new FileReader(noteBasePath)){
+            Gson gson = new Gson();
+            Type collectionType = new TypeToken<ArrayList<Note>>(){}.getType();
+            notes = gson.fromJson(reader, collectionType);
+            return notes;
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        return notes;
     }
 
     public void addNote() {
 
     }
 
-    public JSONObject objectToJsonObject(Note note) {
-        JSONArray date = new JSONArray();
-        JSONObject jsonObject = new JSONObject();
-
-        jsonObject.put("id", String.valueOf(note.getId()));
-        jsonObject.put("theme", note.getTheme());
-
-        date.put("year: " + note.getDate().get(Calendar.YEAR));
-        date.put("month: " + note.getDate().get(Calendar.MONTH));
-        date.put("day: " + note.getDate().get(Calendar.DAY_OF_WEEK));
-
-        jsonObject.put("date", date);
-
-        jsonObject.put("email", note.getEmail());
-        jsonObject.put("message", note.getMessage());
-
-        return jsonObject;
+    public String objectToJsonObject(ArrayList<Note> notes) {
+        Gson gson = new Gson();
+        return gson.toJson(notes);
     }
 }
