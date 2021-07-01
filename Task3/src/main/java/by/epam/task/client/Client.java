cfb5b456a19e8f0225b25d126cad541340b82360
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-07-01 13:47:40 +0300

    Task's done

---
+++ Task3/src/main/java/by/epam/task/client/Client.java
@@ -0,0 +1,12 @@
+package by.epam.task.client;
+
+public class Client {
+    public static final String HOST = "localhost";
+    public static final int PORT = 8089;
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
---
+++ Task3/src/main/java/by/epam/task/server/Server.java
@@ -0,0 +1,9 @@
+package by.epam.task.server;
+
+public class Server {
+    public static final int PORT = 8089;
+
+    public static void main(String[] args) {
+
+    }
+}
