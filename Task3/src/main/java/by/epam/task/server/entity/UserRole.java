de783961b5d0bff9731879b0738a27619153a3c9
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-07-07 23:59:06 +0300

    Task's done

--- Task3/src/main/java/by/epam/task/server/entity/UserRole.java
+++ Task3/src/main/java/by/epam/task/server/entity/UserRole.java
@@ -1,5 +1,12 @@
 package by.epam.task.server.entity;
 
 public enum UserRole {
-    USER, ADMINISTRATOR
+    USER ("User"),
+    ADMINISTRATOR ("Administrator");
+
+    String name;
+
+    UserRole(String name) {
+        this.name = name;
+    }
 }
