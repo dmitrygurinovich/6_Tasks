deae9b709a33d94ab5410e64dfb1f176d854a58d
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-07-14 19:48:34 +0300

    Task's done

--- Task3/src/main/java/by/epam/task/server/logic/Server.java
+++ Task3/src/main/java/by/epam/task/server/logic/Server.java
@@ -1,5 +1,9 @@
 package by.epam.task.server.logic;
 
+import by.epam.task.server.entity.File;
+import by.epam.task.server.storage.FilesBase;
+import by.epam.task.server.view.View;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,18 +14,38 @@ public class Server {
     public static Socket socket;
     public static BufferedReader in;
     public static PrintWriter out;
+    public static Server instance;
 
-    public static void main(String[] args) {
+    private Server() {
+
+    }
+
+    public static Server getInstance() {
+        if (instance == null) {
+            instance = new Server();
+        }
+        return instance;
+    }
+
+    public void runServer() {
+        View view;
+
+        view = new View();
+
         try {
             serverSocket = new ServerSocket(PORT);
-            System.out.println("Server is working...");
+            view.print("Server is working...");
             while (true) {
                 try {
                     socket = serverSocket.accept();
                     in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
                     String line = in.readLine();
-                    System.out.println("Message from client: " + line);
+                    if (line.equals("showFiles")) {
+                        for (File file : FilesBase.getInstance().getFiles()) {
+                            System.out.println(file);
+                        }
+                    }
                     out.println("Your message \"" + line + "\" delivered");
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -36,4 +60,6 @@ public class Server {
             e.printStackTrace();
         }
     }
+
 }
+
--- Task3/src/main/java/by/epam/task/server/main/Main.java
+++ Task3/src/main/java/by/epam/task/server/main/Main.java
@@ -1,18 +1,9 @@
 package by.epam.task.server.main;
 
-import by.epam.task.server.entity.User;
-import by.epam.task.server.logic.FilesBaseLogic;
-import by.epam.task.server.logic.UserBaseLogic;
-import by.epam.task.server.storage.UsersBase;
-import by.epam.task.server.view.View;
-import nu.xom.ParsingException;
-
-import java.io.IOException;
+import by.epam.task.server.logic.Server;
 
 public class Main {
-    public static void main(String[] args){
-        FilesBaseLogic logic = new FilesBaseLogic();
-        View view = new View();
-        UserBaseLogic userBaseLogic = new UserBaseLogic();
+    public static void main(String[] args) {
+        Server.getInstance().runServer();
     }
 }
