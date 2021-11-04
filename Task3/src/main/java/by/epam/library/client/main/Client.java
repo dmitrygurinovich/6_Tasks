59721eafed3dcd9f505a0ce0fdf342b2eea600c8
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-11-04 22:06:26 +0300

    Add files via upload

--- Task3/src/main/java/by/epam/library/client/main/Client.java
+++ Task3/src/main/java/by/epam/library/client/main/Client.java
@@ -8,9 +8,6 @@ public class Client {
     private static Client instance;
     private static final String HOST = "localhost";
     private static final int PORT = 8089;
-    private static BufferedReader in;
-    private static PrintWriter out;
-    private static Socket socket;
 
     private Client() {}
 
@@ -22,32 +19,24 @@ public class Client {
     }
 
     public void startClient() {
-        try {
-            while (true) {
-                try {
-                    socket = new Socket(HOST, PORT);
-                    Scanner reader = new Scanner(System.in);
-                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                    System.out.println("Enter command for Server:");
-                    String line = reader.nextLine();
-                    System.out.println("Your message: " + line);
-                    out.println(line);
+        while (true) {
+            try (Socket socket = new Socket(HOST, PORT);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true))
+            {
+                Scanner reader = new Scanner(System.in);
+                System.out.println("Enter command for Server:");
+                String line = reader.nextLine();
+                System.out.println("Your message: " + line);
+                out.println(line);
 
-                    String response = in.readLine();
+                String response = in.readLine();
 
-                    System.out.println(response);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    break;
-                } finally {
-                    socket.close();
-                    in.close();
-                    out.close();
-                }
+                System.out.println(response);
+            } catch (IOException exception) {
+                System.err.println("Server is not available now!\nPlease, start server!");
+                break;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
