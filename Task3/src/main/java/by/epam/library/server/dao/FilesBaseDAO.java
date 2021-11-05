5163d661d56bbf8ab70ff0b7c0c596297f95db8c
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-11-05 20:04:58 +0300

    Add files via upload

--- Task3/src/main/java/by/epam/library/server/dao/FilesBaseDAO.java
+++ Task3/src/main/java/by/epam/library/server/dao/FilesBaseDAO.java
@@ -1,13 +1,11 @@
 package by.epam.library.server.dao;
 
 import by.epam.library.server.bean.File;
-import nu.xom.ParsingException;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public interface FilesBaseDAO {
-    ArrayList<File> readFilesFromXml() throws ParsingException, IOException;
+    ArrayList<File> readFilesFromXml();
     ArrayList<File> parseXmlToTheListOfFiles(String xml);
     ArrayList<File> getFiles();
     StringBuilder getXmlElement(File file);
